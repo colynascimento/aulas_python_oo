d05f280df54aea74e304c1be5db29f9e485187f8
colynascimento <colymsnascimento@gmail.com>
2024-08-23 21:23:19 -0300

    aula002_getters_e_setters

---
+++ aula002_getters_e_setters/aula002_exemplos_simples.py
@@ -0,0 +1,66 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma 0152
+# Autor: Colyana Magalhães da Silva Nascimento
+# Data: 23/08/2024
+# Aula 002 - Métodos Get e Set: Métodos Assessores
+
+import os
+import math
+
+
+class Equacao:
+    def __init__(self, a = 1, b = 0, c = 0):
+        self._a = a
+        self._b = b
+        self._c = c
+        
+    # Getters
+    def get_a(self):
+        return self._a
+    
+    def get_b(self):
+        return self._b
+    
+    def get_c(self):
+        return self._c
+    
+    # Setters
+    def set_a(self, value):
+        self._a = value
+            
+    def set_b(self, value):
+        self._b = value
+        
+    def set_c(self, value):
+        self._c = value
+            
+    def calcular_raizes(self):
+        
+        # Calculando o delta
+        delta = self._b ** 2 - 4 * self._a * self._c
+        
+        if delta < 0:
+            return 'A equação não possui raízes reais.'
+        elif delta == 0:
+            raiz = (-self._b + math.sqrt(delta)) / (2 * self._a)
+            return f'A equação possui uma raiz real: {raiz}'
+        else:
+            x1 = (-self._b + math.sqrt(delta)) / (2 * self._a)
+            x2 = (-self._b + math.sqrt(delta)) / (2 * self._a)
+        return f'A equação possui duas raízes reais: {x1} e {x2}'
+    
+os.system('cls')
+
+print('-' * 70)
+print('Cálculo de equação do 2º Grau')
+print('=' * 70)
+equacao1 = Equacao(1, -3, 2)
+print(equacao1.calcular_raizes())
+
+print('-' * 70)
+print('Cálculo de equação do 2º Grau')
+equacao1.set_a()
+equacao1.set_b()
+equacao1.set_c()
+print(equacao1.calcular_raizes())
+print('-' * 70)
