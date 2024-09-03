54fc01346e78b1bc298ed9578ec9754469e87a90
Coly <colymsnascimento@gmail.com>
2024-09-03 17:43:07 -0300

    atividade001 b

--- !atividades/atividade001_classe_e_objeto/b.py
+++ !atividades/atividade001_classe_e_objeto/b.py
@@ -5,3 +5,44 @@
 # Atividade 001 - Classe e Objeto
 
 # B) Faça um programa que peça o ano do seu nascimento e calcule a sua idade.
+
+import os
+from datetime import datetime
+
+
+class Idade:
+    def __init__(self, data_nascimento):
+        self.data_nascimento = data_nascimento
+
+    def calcular(self):
+        hoje = datetime.now()
+        data_nascimento = self.data_nascimento
+        data_nascimento = datetime.strptime(data_nascimento, '%d/%m/%Y')
+        self._idade = hoje.year - data_nascimento.year - ((hoje.month, hoje.day) < (data_nascimento.month, data_nascimento.day))
+
+        return self._idade
+    
+    def validar_data(self):
+        try:
+            datetime.strptime(self.data_nascimento, '%d/%m/%Y')
+            return True
+        except ValueError:
+            return False
+    
+os.system('cls')
+
+while True:
+    print('_' * 60)
+    print('\nSeja bem-vindo ao programa!')
+    print('Vamos verificar sua idade.')
+    print('_' * 60)
+    
+    data_nascimento = input('Insira sua data de nascimento (DD/MM/YYYY): ')
+    
+    idade = Idade(data_nascimento)
+    
+    if idade.validar_data():
+        print(f'{idade.calcular()}')
+    else:
+        os.system('cls')
+        print('Digite uma data no formato DD/MM/YYYY para validar!\n')
