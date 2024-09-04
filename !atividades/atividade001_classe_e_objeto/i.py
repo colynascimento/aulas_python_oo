6fd5b6b1735dc1c733da227d07ac31dc5b0c9f27
Coly <colymsnascimento@gmail.com>
2024-09-04 17:44:50 -0300

    atividade001 j

--- !atividades/atividade001_classe_e_objeto/i.py
+++ !atividades/atividade001_classe_e_objeto/i.py
@@ -14,4 +14,16 @@ import os
 class ConversorDeMoedas:
     def __init__(self, reais):
         self.reais = reais
-    
+
+    def converter_para_dolar(self):
+        dolares = self.reais / 5.63
+        return dolares
+ 
+os.system('cls')
+
+saldo_real = float(input('Digite o valor em reais: '))
+
+conversor = ConversorDeMoedas(saldo_real)
+saldo_dolar = conversor.converter_para_dolar()
+
+print(F'R${saldo_real:.2f} são equivalentes a U${saldo_dolar:.2f} dólares.')
--- !atividades/atividade001_classe_e_objeto/j.py
+++ !atividades/atividade001_classe_e_objeto/j.py
@@ -1,7 +1,27 @@
 # Curso de Desenvolvimento de Sistemas
 # Turma 0152
 # Autor: Colyana Magalhães da Silva Nascimento
-# Data: 02/09/2024
+# Data: 03/09/2024
 # Atividade 001 - Classe e Objeto
 
-# J) Faça um programa com entrada de dados para calcular o perímetro de um retângulo. 
+# J) Faça um programa com entrada de dados para calcular
+#  o perímetro de um retângulo. 
+
+import os
+
+
+class Retangulo:
+    def __init__(self, comprimento, altura):
+        self.comprimento = comprimento
+        self.altura = altura
+
+    def calcular_perimetro(self):
+        perimetro = self.comprimento * 2 + self.altura * 2
+        return perimetro
+    
+os.system('cls')
+
+comprimento_retangulo = input('Digite o comprimento do retângulo: ')
+altura_retangulo = input('Digite a altura do retângulo: ')
+
+retangulo = Retangulo(comprimento_retangulo, altura_retangulo)
